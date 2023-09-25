9cc1f3f1f445e787b958aa13c1999352c1d07a92
Shailendra Jain <shailendra.jain0894@gmail.com>
2023-09-25 20:14:16 +0530

    Second Typescript Class Array and methods

--- src/app/typescript/typescript.component.ts
+++ src/app/typescript/typescript.component.ts
@@ -69,8 +69,21 @@ export class TypescriptComponent {
     // console.log(typeof(this.isdone2));
 
     //this.showdata("Shailendra ",500);
-    this.variablemethod();
+    // this.variablemethod();
 
+    this.numberMethod();
+    // this.stringMethod();
+    // this.booleanMethod();
+    // this.show();
+    // console.log(this.show);
+
+    // this.nullMethod();
+    // this.anyMethod();
+    //this.sum(10, 20);
+    // this.sum("10","20");
+    // this.sum("hello","hi");
+    //this.arrayMethod();
+    // this.toupleMethod();
 
 }
 
@@ -128,5 +141,201 @@ variablemethod() {
 //   console.log("ID= " + id + "Name= " + name);
 // }
 
+toupleMethod() {
+  // let list: [number, string, number, boolean] = [100, "Ajeet Kumar", 10, true];
+
+  // console.log(list);
+  // console.log(typeof(list));
+
+  var empid: number = 101;
+  var empname: string = "Chandan Kumar";
+
+  let employee: [number, string] = [empid, empname];
+
+  employee = [102, "Ajeet Kumar"];
+  // employee=[103];
+
+  employee.push(103, "Mohan Singh");
+  employee.push("Rohit Kumar", 10);
+  employee.push(105);
+  employee.push(106, "Rahul", 120, "kjhjkl", "jhkljlk");
+
+
+
+  console.log(employee);
+  // console.log(employee[1]);
+
+  let person: [number, string][] = [[1, "Chandan"], [2, "Ajeet"], [3, "Rahul"]];
+
+  console.log(person);
+
+  console.log(person[2][1]);
+
+
+
+}
+
+arrayMethod() {
+  let list1: number[] = [1, 2, 3, 4, 4];
+  let list2: string[] = ["Ram", "Shyam"];
+
+  let list3: Array<number> = [1, 2, 3, 4, 56, 7, 88];
+
+  let list4: any[] = ["hello", 1, "hj", true];
+
+  console.log(list1);
+  console.log(list2);
+  console.log(list3);
+
+  console.log(typeof (list1));
+  console.log(typeof (list2));
+  console.log(typeof (list3));
+
+  console.log(list2[1]);
+
+  list2.push("Mohan");
+  console.log(list2);
+
+
+}
+
+sum(a: number, b: any) {
+  console.log(a + b);
+}
+
+anyMethod() {
+  let a: any;
+
+  console.log(typeof (a));
+
+  a = 10;
+  a = "Hello";
+  a = true;
+  a = 100;
+
+  console.log(typeof (a));
+}
+
+// undefinedMethod() {
+//   let num1: undefined;
+//   let num2 = undefined;
+
+//   // num1=12;
+//   // num1="jk";
+
+//   num1 = null;
+//   num1 = undefined;
+
+//   num2 = 12;
+//   num2 = "hello";
+//   num2 = true;
+
+// }
+
+// nullMethod() {
+//   let num1: null;
+//   let num2 = null;
+
+//   // num1=12;
+//   // num1=";lk;";
+//   num1 = null;
+//   num1 = undefined;
+
+//   num2 = 12;
+//   num2 = "kjkl";
+//   num2 = true;
+
+//   // console.log(typeof(num1));
+//   // console.log(typeof(num2));
+// }
+
+// voidMethod() {
+//   let num1: void;
+
+//   // num1=12;
+//   // num1="Hello";
+//   // num1=true;
+
+//   num1 = null;
+//   num1 = undefined;
+// }
+
+show() {
+  //console.log("Hello");
+  return "Hello";
+}
+
+show2(): void {
+  // return "jkh";
+  console.log("Hi");
+}
+
+show3(): number {
+  return 12;
+}
+
+show4(): string {
+  return "12";
+}
+
+numberMethod() {
+  let num1 = 10;
+  let num2: number = 20;
+  let num3: number = 10.20;
+  let num4 = 20.30;
+
+  let num5: number;
+  //num5="l;k;l"
+  num5 = 90;
+
+  console.log(typeof (num5));
+
+  console.log(num1);
+  console.log(num2);
+  console.log(num3);
+  console.log(num4);
+
+  console.log(typeof (num1));
+  console.log(typeof (num2));
+  console.log(typeof (num3));
+  console.log(typeof (num4));
+}
+
+stringMethod() {
+  let str1 = "Chandan Kumar";
+  let str2: string = "Ajeet Kumar";
+  let str3 = "";
+  let str4 = '';
+
+  console.log(typeof (str1));
+  console.log(typeof (str2));
+  console.log(typeof (str3));
+  console.log(typeof (str4));
+
+  let empname: string = "Chandan Kumar";
+  let empdept: string = "Software Development";
+  //before ES6
+  let output1: string = empname + " works in the " + empdept + " department.";
+
+  //After ES6
+  let output2: string = `${empname} works in the ${empdept} department`;
+
+
+  console.log(output1);
+  console.log(output2);
+}
+
+
+booleanMethod() {
+  let isdone = true;
+  let isdone2: boolean = false;
+
+  // isdone=12;
+  // isdone2="true";
+  // isdone2=false;
+
+  console.log(typeof (isdone));
+  console.log(typeof (isdone2));
+}
 
 }
